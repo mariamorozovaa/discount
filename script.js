e9ea06ce9d6b7e6ee78b99802c375350b440b197
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-10-21 18:07:00 +0300

    Add a functional for promocodes

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 const originalPrice = document.querySelector("#original-price");
 
+const increaseBtn = document.querySelector("#increase-btn");
+const decreaseBtn = document.querySelector("#decrease-btn");
 const quantity = document.querySelector("#quantity");
 
-const plusBtn = document.querySelector("#increase-btn");
-const minusBtn = document.querySelector("#decrease-btn");
-const applyBtn = document.querySelector("#apply-promo-btn");
-const calculateBtn = document.querySelector("#calculate-btn");
-
 const discountPercent = document.querySelector("#discount-percent");
-const promocode = document.querySelector("#promo-code");
+
+const promoCode = document.querySelector("#promo-code");
+const applyPromoBtn = document.querySelector("#apply-promo-btn");
+const promoMessage = document.querySelector("#promo-message");
+
 const loyaltyLevel = document.querySelector("#loyalty-level");
 
-const result = document.querySelector("#result");
+const calculateBtn = document.querySelector("#calculate-btn");
 
+const result = document.querySelector("#result");
 const baseTotal = document.querySelector("#base-total");
-const productDiscountValue = document.querySelector("#product-discount-value");
-const promoDiscountValue = document.querySelector("#promo-discount-value");
-const loyaltyDiscountValue = document.querySelector("#loyalty-discount-value");
+const productDiscountValue = document.querySelector("#product-discount-value"); //- Скидка на товар (строка `product-discount-row`, значения `discount-percent-text`, `product-discount-value`)
+const promoDiscountValue = document.querySelector("#promo-discount-value"); //- Промокод (строка `promo-discount-row`, значения `promo-code-text`, `promo-discount-value`)
+const loyaltyDiscountValue = document.querySelector("#loyalty-discount-value"); //- Скидка лояльности (строка `loyalty-discount-row`, значения `loyalty-discount-text`, `loyalty-discount-value`)
 const totalSavings = document.querySelector("#total-savings");
 const finalPrice = document.querySelector("#final-price");
-const cashbackValue = document.querySelector("#cashback-value");
+const cashbackValue = document.querySelector("#cashback-value"); //- Кэшбэк (строка `cashback-row`, значение `cashback-value`)
 const freeShippingBadge = document.querySelector("#free-shipping-badge");
 
+const errors = document.querySelector("#error");
+
+const DISCOUNT_PERCENTS = {
+  none: { discount: 0, description: "Без скидки" },
+  season: { discount: 5, description: "Сезонная распродажа" },
+  blackFriday: { discount: 10, description: "Black Friday" },
+  bithdayStore: { discount: 15, description: "День рождения магазина" },
+  newYear: { discount: 20, description: "Новогодняя акция" },
+  liquidation: { discount: 30, description: "Ликвидация остатков" },
+  markdown: { discount: 50, description: "Уценка" },
+};
+
 const PROMO_CODES = {
-  WELCOME2024: { type: "fixed", value: 500, description: "Скидка 500руб для новых клиентов" },
+  WELCOME2024: { type: "fixed", value: 500, description: "Скидка 500₽ для новых клиентов" },
   FREESHIP: { type: "shipping", value: 0, description: "Бесплатная доставка" },
-  SAVE10: { type: "percent", value: 10, description: "скидка 10%" },
-  VIP20: { type: "percent", value: 20, description: "скидка 20% для вип клиентов" },
+  SAVE10: { type: "percent", value: 10, description: "Дополнительная скидка 10%" },
+  VIP20: { type: "percent", value: 20, description: "Скидка 20% для VIP клиентов" },
 };
 
 const LOYALTY_LEVELS = {
-  none: { cashback: 0, extradiscount: 0 },
-  bronze: { cashback: 3, extradiscount: 0 },
-  silver: { cashback: 5, extradiscount: 2 },
-  gold: { cashback: 7, extradiscount: 5 },
+  none: { cashback: 0, extraDiscount: 0 },
+  bronze: { cashback: 3, extraDiscount: 0 },
+  silver: { cashback: 5, extraDiscount: 2 },
+  gold: { cashback: 7, extraDiscount: 5 },
 };
-
-let applyedPromocode = null;
 
 function increaceQuantity() {
   let currValue = parseInt(quantity.value);
@@ -52,5 +64,67 @@ function decreaceQuantity() {
   }
 }
 
-plusBtn.addEventListener("click", increaceQuantity);
-minusBtn.addEventListener("click", decreaceQuantity);
+increaseBtn.addEventListener("click", increaceQuantity);
+decreaseBtn.addEventListener("click", decreaceQuantity);
+
+let applyedPromoCode = null;
+
+function applyPromoCode() {
+  let parsedPromoCode = promoCode.value.trim().toUpperCase();
+
+  if (!parsedPromoCode) {
+    promoMessage.classList.remove("hidden");
+    promoMessage.textContent = "";
+    const promoMessageError = document.createElement("p");
+    promoMessageError.textContent = "Введите промокод";
+    promoMessage.appendChild(promoMessageError);
+    return;
+  } else {
+    promoMessage.classList.add("hidden");
+    promoMessage.textContent = "";
+  }
+
+  const currPromoCode = PROMO_CODES[parsedPromoCode]; //новое
+
+  if (currPromoCode) {
+    applyedPromoCode = parsedPromoCode;
+    promoMessage.classList.remove("hidden");
+    promoMessage.textContent = "";
+    const promoMessageApply = document.createElement("p");
+    promoMessageApply.textContent = "Промокод применен";
+    promoMessageApply.classList.add("green-text");
+    promoMessage.appendChild(promoMessageApply);
+    promoCode.disabled = true; //новое
+    applyPromoBtn.textContent = "Отменить";
+    applyPromoBtn.classList.add("red-bcg");
+  } else {
+    promoMessage.classList.remove("hidden");
+    promoMessage.textContent = "";
+    const promoMessageIncorrect = document.createElement("p");
+    promoMessageIncorrect.textContent = "Промокод не найден";
+    promoMessage.appendChild(promoMessageIncorrect);
+    promoMessageIncorrect.classList.add("red-text");
+    applyedPromoCode = null;
+  }
+}
+
+function resetPromoCode() {
+  applyedPromoCode = null;
+  promoCode.value = "";
+  promoCode.disabled = false;
+  promoMessage.classList.add("hidden");
+  applyPromoBtn.textContent = "Применить";
+  applyPromoBtn.classList.remove("red-bcg");
+}
+
+function showPromoMessage(message, type) {
+  // - Установи текст сообщения
+  // - Добавь нужный класс ('success' или 'error')
+  // - Убери класс 'hidden' чтобы показать сообщение
+}
+
+applyPromoBtn.addEventListener("click", () => {
+  if (applyPromoBtn.textContent === "Применить") {
+    applyPromoCode();
+  } else resetPromoCode();
+});
